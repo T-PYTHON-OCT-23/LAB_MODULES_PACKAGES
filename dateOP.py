4c4da085af9022049bf8995a24e5dda3bf77293a
AlbandariAljabri <Albandarialjabri@gmail.com>
2023-10-20 14:43:07 +0300

    lab module&package solved

---
+++ dateOP.py
@@ -0,0 +1,8 @@
+### hint : You should import the date class from the datetime module.
+import datetime
+
+
+def date_time():
+
+    current_time = datetime.datetime.now()
+    return print(f"The time is  {current_time}")
---
+++ main.py
@@ -0,0 +1,4 @@
+from dateOP import date_time
+
+
+date_time()
